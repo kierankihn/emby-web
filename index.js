275c2a7bfc02bd86df75e12f254a751fe3446802
Kieran Kihn <114803508+kierankihn@users.noreply.github.com>
2023-12-21 13:27:43 +0800

    feat: updated user-agent

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-const { app, BrowserView, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron')
 
-app.whenReady().then(() => {
+const createWindow = () => {
 
-    const win = new BrowserWindow({ width: 1920, height: 1080 })
-
-    win.on('closed', function () {
-        win = null
+    const win = new BrowserWindow({
+        autoHideMenuBar: true,
     })
 
-    const view = new BrowserView()
-    win.setBrowserView(view)
-    view.setBounds({ x: 0, y: 0, width: 1920, height: 1080 })
-    view.webContents.loadURL('https://emby.kierankihn.com/web/index.html#!/music?serverId=dfec84876c6441d684d6baaa4c662879&parentId=6588&tab=songs')
+    win.maximize()
+
+    win.loadURL('https://emby.kierankihn.com/web/index.html#!/music?serverId=dfec84876c6441d684d6baaa4c662879&parentId=6588&tab=songs', { userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36' })
+}
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
 })
